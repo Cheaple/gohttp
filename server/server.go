dc187cf420efabd60d47611bf958f8ad96e6b7fa
Cheaple <chenyulechn@gmail.com>
2023-11-13 12:52:21 +0100

    re-struct

---
+++ server/server.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"../utils"
+)
+
+var (
+	debug			bool = false
+	port			int = 8080
+	mediaType		= [...]string{"text/html", "text/plain", "image/gif", "image/jpeg", "image/jpeg", "text/css"}
+	fileType		= [...]string{"html", "txt", "gif", "jpeg", "jpg", "css"}
+	workspace string
+	maxWorkers		= 10
+	var workerSem	= make(chan struct{}, maxWorkers)
+)
+
+func main() {
+	_, src, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Println("Cannot access path of workspace")
+		return
+	}
+	workspace = filepath.Dir(src) + "/data"
+
+	flag.IntVar(&port, "port", 8080, "Port to listen on")
+	flag.IntVar(&port, "p", 8080, "Port to listen on")
+	flag.Parse()
+	
+	// Create a proxy server listening on, listen on the port specified from the command line
+	address := fmt.Sprintf(":%d", port)
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Printf("Fatal error listening on port %s: %s", strport, err.Error())
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	log.Printf("Proxy server is listening on %d\n", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		log.Println("Receiving a new request from", conn.RemoteAddr().String())
+		go handleRequest(conn)
+	}
+}
+
+
+//
+// Handler for incoming request from HTTP connection
+//
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	workerSem <- struct{}{}
+	defer func() { <-seworkerSemm }()
+	log.Println("Handling a new connection from ", clientConn.RemoteAddr().String())
+
+	request, err := parseRequest(conn)
+	if err != nil {
+		fmt.Println("Error parsing connection:", err)
+		return
+	}
+
+	// Check the request method
+	if request.Method == "GET" {
+		handle(request)
+	} else if request.Method == "POST" {
+		returnBadRequest(conn)
+	} else {
+		// Return a "501 Not Implemented" response for non-GET requests
+		log.Println("Error forwarding: not implemented method", request.Method)
+		returnNotImplement(clientConn)
+		return
+	}
+}
+
+func typeMatch(typeList [6]string, target string) (bool, string) {
+	for i, str := range typeList {
+		if str == target {
+			return true, mediaType[i]
+		}
+	}
+	return false, ""
+}
+
+//
+// parse HTTP connection into a http.Request object
+//
+func parseRequest(conn net.Conn) (*http.Request, error) {
+	reader := bufio.NewReader(conn)
+
+	// Read the request line
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	parts := strings.Fields(requestLine)
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid request line: %s", requestLine)
+	}
+	reqURL, err := url.Parse(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("error parsing URL: %v", err)
+	}
+
+	req := &http.Request{
+		Method: parts[0],
+		URL: reqURL,
+		Proto: "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+
+	req.Header = make(http.Header)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil || line == "\r\n" {
+			break
+		}
+
+		headerParts := strings.SplitN(line, ":", 2)
+		if len(headerParts) == 2 {
+			req.Header.Add(strings.TrimSpace(headerParts[0]), strings.TrimSpace(headerParts[1]))
+		}
+	}
+
+	contentLengthStr := req.Header.Get("Content-Length")
+	if contentLengthStr != "" {
+		var contentLength int
+		fmt.Sscanf(contentLengthStr, "%d", &contentLength)
+
+		// Read the request body
+		bodyBytes := make([]byte, contentLength)
+		_, err := io.ReadFull(reader, bodyBytes)
+
+		if err != nil {
+			return nil, fmt.Errorf("error reading request body: %v", err)
+		}
+		req.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	}
+
+	return req, nil
+}
+
+
+
+func handlePOST(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	
+	// Check if the content type is "image"
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "image/jpeg" {
+		// http.Error(w, "Unsupported media type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	file, err := os.Create("received_image.jpg")
+	if err != nil {
+		// http.Error(w, "Error creating file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// Read the request body and write it to the local file
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		// http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Received image with content type:", contentType)
+	log.Println("Image size:", len(file), "bytes")
+
+	// You can handle the image data here, such as saving it to a file or processing it.
+
+	// // Send a response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Image received successfully"))
+}
+
+func returnNotImplement(conn net.Conn) {
+	var buf string
+
+	buf = "HTTP/1.1 501 Method Not Implemented\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Content-Type: text/html; charset=utf-8\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Connection: close\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<HTML><HEAD><TITLE>Method Not Implemented\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</TITLE></HEAD>\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<BODY><P>HTTP request method not supported.\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</BODY></HTML>\r\n"
+	_, _ = conn.Write([]byte(buf))
+}
+
+func returnNotFound(conn net.Conn) {
+	var buf string
+
+	buf = "HTTP/1.1 404 Not Found\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Content-Type: text/html; charset=utf-8\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Connection: close\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<HTML><HEAD><TITLE>404 Not Found\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</TITLE></HEAD>\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<BODY><P>404 not Found.\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</BODY></HTML>\r\n"
+	_, _ = conn.Write([]byte(buf))
+}
+
+func returnBadRequest(conn net.Conn) {
+	var buf string
+
+	buf = "HTTP/1.1 400 Bad Request\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Content-Type: text/html; charset=utf-8\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "Connection: close\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<HTML><HEAD><TITLE>400 Bad Request\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</TITLE></HEAD>\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "<BODY><P>400 Bad Request.\r\n"
+	_, _ = conn.Write([]byte(buf))
+	buf = "</BODY></HTML>\r\n"
+	_, _ = conn.Write([]byte(buf))
+}
+
+
---
+++ utils/util.go
@@ -0,0 +1,111 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+// ConnResponseWriter implements the http.ResponseWriter interface for net.Conn.
+type ConnResponseWriter struct {
+	conn   net.Conn
+	header http.Header
+	status int
+}
+
+
+// NewConnResponseWriter creates a new ConnResponseWriter.
+func NewConnResponseWriter(conn net.Conn) *ConnResponseWriter {
+	return &ConnResponseWriter{
+		conn:   conn,
+		header: make(http.Header),
+		status: http.StatusOK,
+	}
+}
+
+
+// Header returns the header map.
+func (w *ConnResponseWriter) Header() http.Header {
+	return w.header
+}
+
+
+// Write writes the data to the connection.
+func (w *ConnResponseWriter) Write(data []byte) (int, error) {
+	if w.status == 0 {
+		w.WriteHeader(http.StatusOK)
+	}
+	return w.conn.Write(data)
+}
+
+
+// WriteHeader writes the status code to the connection.
+func (w *ConnResponseWriter) WriteHeader(statusCode int) {
+	if w.status == 0 {
+		w.status = statusCode
+		statusText := http.StatusText(statusCode)
+		fmt.Fprintf(w.conn, "HTTP/1.1 %d %s\r\n", statusCode, statusText)
+		w.header.Write(w.conn)
+		fmt.Fprintf(w.conn, "\r\n")
+	}
+}
+
+
+// parse HTTP connection into a http.Request object
+func parseRequest(conn net.Conn) (*http.Request, error) {
+	reader := bufio.NewReader(conn)
+
+	// Read the request line
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	parts := strings.Fields(requestLine)
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid request line: %s", requestLine)
+	}
+	reqURL, err := url.Parse(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("error parsing URL: %v", err)
+	}
+
+	req := &http.Request{
+		Method: parts[0],
+		URL: reqURL,
+		Proto: "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+
+	req.Header = make(http.Header)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil || line == "\r\n" {
+			break
+		}
+
+		headerParts := strings.SplitN(line, ":", 2)
+		if len(headerParts) == 2 {
+			req.Header.Add(strings.TrimSpace(headerParts[0]), strings.TrimSpace(headerParts[1]))
+		}
+	}
+
+	contentLengthStr := req.Header.Get("Content-Length")
+	if contentLengthStr != "" {
+		var contentLength int
+		fmt.Sscanf(contentLengthStr, "%d", &contentLength)
+
+		// Read the request body
+		bodyBytes := make([]byte, contentLength)
+		_, err := io.ReadFull(reader, bodyBytes)
+
+		if err != nil {
+			return nil, fmt.Errorf("error reading request body: %v", err)
+		}
+		req.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	}
+
+	return req, nil
+}
